9a46d50fafbe1ee4765100ae1f45c42864b2f421
Gonzalez Sanchez Sergio De Jesus <XM06371@cb.bbvabancomer.com.mx>
2017-03-29 16:30:09 -0600

    PRIMER COMMIT

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package softtek.git.proyecto01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "Hello Git!";
+		
+		 System.out.println( " " + string + " ");
+
+	}
+
+}
