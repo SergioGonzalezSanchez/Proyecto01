6868469ca8e83b321634eaf9bd277af8b4d56de0
Gonzalez Sanchez Sergio De Jesus <XM06371@cb.bbvabancomer.com.mx>
2017-03-29 17:29:36 -0600

    Ultima Version

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String string = "Hello Git a bug was fixed!!";
+		String string = "Hello Git and GitHub, a bug was fixed!!";
 		
 		 System.out.println( " " + string + " ");
 
